555168e2a58b9137d5de7884f622b3d2430ab250
MounikaPunganuru <punganurumounika@gmail.com>
2025-06-04 16:11:34 +0530

    Add files via upload

---
+++ StaticInputString2.java
@@ -0,0 +1,11 @@
+public class StaticInputString2
+{
+	public static void main(String[] args) {
+	    String a="mounika";
+	    for(int i=0;i<a.length();i++)
+	    {
+	        	System.out.println(a.charAt(i));
+	    }
+		
+	}
+}

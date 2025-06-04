314efe12ecb454e88bb5bc5cbbebc3a11a22e7e1
MounikaPunganuru <punganurumounika@gmail.com>
2025-06-04 16:11:59 +0530

    Add files via upload

---
+++ StaticInputString3.java
@@ -0,0 +1,11 @@
+public class StaticInputString3
+{
+	public static void main(String[] args) {
+	    String a=new String("mounika");
+	    for(int i=0;i<a.length();i++)
+	    {
+	        	System.out.println(a.charAt(i));
+	    }
+		
+	}
+}
